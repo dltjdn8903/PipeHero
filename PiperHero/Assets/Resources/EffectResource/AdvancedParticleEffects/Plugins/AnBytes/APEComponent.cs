f5a45fb3240ce4a793187cef1bb1fda76e0495c2
dltjdn8903 <dltjdn8903@naver.com>
2016-09-01 01:17:50 +0900

    All File Delete

--- PiperHero/Assets/Resources/EffectResource/AdvancedParticleEffects/Plugins/AnBytes/APEComponent.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-using System.Collections;
-
-public class APEComponent : MonoBehaviour {
-	public int ID = -1;
-}
--- PiperHero/Assets/Resources/EffectResource/AdvancedParticleEffects/other/Sky/fix.js
+++
@@ -1,5 +0,0 @@
-var gameobj1:GameObject;
-var gameobj2:GameObject;
-function Update () {
-gameobj1.transform.position=gameobj2.transform.position;
-}
